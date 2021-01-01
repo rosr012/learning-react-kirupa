f1720a68ae1b782af7597633e8787676e50a77d0
kyn <rosr012@naver.com>
2021-01-01 20:07:29 +0900

    init commit

---
+++ slidingmenu/src/MenuButton.js
@@ -0,0 +1,21 @@
+import React, {PureComponent} from "react";
+import './MenuButton.css';
+
+class MenuButton extends PureComponent{
+    // shouldComponentUpdate(nextProps, nextState, nextContext) {
+    //     if (nextProps.handleMouseDown===this.props.handleMouseDown){
+    //         return false;
+    //     }else{
+    //         return true;
+    //     }
+    // }
+
+    render(){
+        console.log("Rendering: MenuButton");
+        return(
+            <button id="roundButton" onMouseDown={this.props.handleMouseDown}></button>
+        );
+    }
+}
+
+export default MenuButton;
